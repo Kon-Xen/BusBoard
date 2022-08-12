f6a75d7f4491170bce74c7442be2ea9a2ad24f66
Konstantinos <kon.xen@gmail.com>
2022-08-12 11:09:42 +0100

    got the data out the async function

--- Request.js
+++ Request.js
@@ -1,30 +1,47 @@
 //ask user for a stop code, and print a list of the next five buses at that stop code,
 // with their routes, destinations, and the time until they arrive in minutes. []
 
-// import fetch from 'node-fetch';
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-import readline from 'readline';
+import fetch from 'node-fetch';
+import * as readline from 'readline';
 
 //todo
 // console.log("Enter bus stop code ");
 // let input = readline.createInterface( process.stdin, process.stdout);
 
-const stop = "490008660N";
-// let findStop = "https://api.tfl.gov.uk/StopPoint/" + stop;
-let findStop = 'https://api.tfl.gov.uk/StopPoint/Mode/bus'
-let findAllstops = "https://api.tfl.gov.uk/StopPoint/" + stop;
+//  const reader = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
 
-function doQuery(url) {
-    return fetch(url)
+ function getIdFromCLI(){
+     readline.question(`enter id`, id => {
+         console.log(`you enterd ${ id }!`);
+         readline.close();
+     });
+ }
+
+
+
+const stopID = "490008660N";
+
+let findStop = 'https://api.tfl.gov.uk/StopPoint/Mode/bus'
+
+let FindStops = "https://api.tfl.gov.uk/StopPoint/" + stopID;
+
+//Gets the list of arrival predictions for the given stop point id
+let findArrivals = "https://api.tfl.gov.uk/StopPoint/"+ stopID +"/Arrivals\n";
+
+async function doQuery(url) {
+    const response =  await fetch(url)
         .then(response => response.json())
-        // return response
-        .then(body => console.log(body));
+        .then(data =>{return data});
+        return response;
 }
 
-//Gets a distinct list of disrupted stop points for the given modes
 
-let data = doQuery(findStop);
+let data = doQuery(FindStops)
+    .then (response => console.log(response));
 
 // console.log(typeof data);
-
+//
 
